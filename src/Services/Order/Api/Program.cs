02e512785293ee008a1516ef0e2899e9d009c91b
László Séra <LSera@graphisoft.com>
2022-03-23 08:30:55 +0100

    Added another example of deleting an outbox event from the database immediately after saving while the notification is still running (due to CDC)

--- src/Services/Order/Api/Program.cs
+++ src/Services/Order/Api/Program.cs
@@ -23,17 +23,50 @@ app.MapPost("/orders", async (PurchaseOrder order, OrderDbContext db) =>
 {
     db.Orders.Add(order);
     
-    var outboxEvent = new Outbox(
-        aggregateType: "order", 
-        aggregateId: order.Id, 
-        type: "OrderCreated",
-        payload: JsonSerializer.Serialize(new OrderCreatedEvent(order))
-    );
-    db.OutboxEvents.Add(outboxEvent);
+    AddOutboxEvent(order, db);
     
     await db.SaveChangesAsync();
 
     return Results.Created($"/orders/{order.Id}", order);
+});
+
+app.MapPost("/orders/no-additional-space", async (PurchaseOrder order, OrderDbContext db) =>
+{
+    await using var transaction = db.Database.BeginTransaction();
+    
+    try
+    {
+        db.Orders.Add(order);
+
+        var outboxEvent = AddOutboxEvent(order, db);
+
+        // Persist the order and outboxEvent entities
+        await db.SaveChangesAsync();
+
+        // Remove the persisted outboxEvent immediately, so we will create an INSERT and a DELETE entry in the log
+        // once the transaction commits.
+        //
+        // After that, Debezium will process these events:
+        //    - for any INSERT, a message with the event’s payload will be sent to Apache Kafka.
+        //    - DELETE events on the other hand can be ignored, does not require any propagation to the message broker.
+        //
+        // This means that no additional disk space is needed for the table
+        // (apart from the log file elements which will automatically be discarded at some point).
+        RemoveOutboxEvent(db, outboxEvent);
+        await db.SaveChangesAsync();
+
+        // Commit transaction if all commands succeed, transaction will auto-rollback
+        // when disposed if either commands fails
+        await transaction.CommitAsync();
+
+        return Results.Created($"/orders/{order.Id}", order);
+    }
+    catch (Exception exc)
+    {
+        await transaction.RollbackAsync();
+
+        return Results.Problem(exc.Message);
+    }
 });
 
 app.MapGet("/orders/{id}", async (Guid id, OrderDbContext db) =>
@@ -44,5 +77,24 @@ app.MapGet("/orders/{id}", async (Guid id, OrderDbContext db) =>
         ? Results.Ok(order)
         : Results.NotFound();
 });
+
+Outbox AddOutboxEvent(PurchaseOrder purchaseOrder, OrderDbContext orderDbContext)
+{
+    var outbox = new Outbox(
+        aggregateType: "order",
+        aggregateId: purchaseOrder.Id,
+        type: "OrderCreated",
+        payload: JsonSerializer.Serialize(new OrderCreatedEvent(purchaseOrder))
+    );
+    
+    orderDbContext.OutboxEvents.Add(outbox);
+    
+    return outbox;
+}
+
+void RemoveOutboxEvent(OrderDbContext orderDbContext, Outbox outbox)
+{
+    orderDbContext.OutboxEvents.Remove(outbox);
+}
 
 app.Run();
